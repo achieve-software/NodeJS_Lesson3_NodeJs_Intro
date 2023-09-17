44011d530bbb7a20706c72a8a3938731a4737a9c
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-18 00:34:31 +0300

    req res

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const http = require("node:http"); // builtin: https://nodejs.org/api/http.html
+
+// http.createServer((req, res) => { ... }
+const app = http.createServer((request, response) => {
+  response.end("<h1> Welcome to NodeJS Server </h1>");
+});
+
+app.listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
