52abd74516c3c5cf8fba0a687406b1f9fd8c9e47
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-18 01:23:40 +0300

    url

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 const http = require("node:http"); // builtin: https://nodejs.org/api/http.html
 
 // http.createServer((req, res) => { ... }
-const app = http.createServer((request, response) => {
-  response.end("<h1> Welcome to NodeJS Server </h1>");
-});
+// const app = http.createServer((request, response) => {
+//   response.end("<h1> Welcome to NodeJS Server </h1>");
+// });
 
-app.listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
+// app.listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
+
+// ----------------------------------------------------------------------------------------
+
+const app = http
+  .createServer((req, res) => {
+    // console.log(req);
+    // console.log(res);
+    console.log(req.url)
+    if(req.url=='/'){
+      res.end("main page")
+    }
+    else if(req.url=="/second"){
+      res.end("second page")
+    }
+    else{
+      res.end("Server is running");
+    }
+
+    
+  })
+  .listen(8000, () => console.log("Server Runned: http://127.0.0.1:8000"));
